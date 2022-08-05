904334314c65aab9a412f0c838ecf226359bbbd1
Urmil07 <urmilpariyawala7@gmail.com>
2022-08-05 14:30:48 +0530

    delete function fixed

--- app/Modules/Teacher_Dashboard/Config/Routes.php
+++ app/Modules/Teacher_Dashboard/Config/Routes.php
@@ -9,4 +9,5 @@ $routes->group('teacher_dashboard', ['namespace' => 'App\Modules\Teacher_Dashboa
     $routes->add('/', 'Teacher_Dashboard::index',['filter' => 'teacherauthFilter']);
     $routes->add('create', 'Teacher_Dashboard::create',['filter' => 'teacherauthFilter']);
     $routes->add('create/(:any)', 'Teacher_Dashboard::create/$1',['filter' => 'teacherauthFilter']);
+    $routes->add('delete/(:any)', 'Teacher_Dashboard::delete/$1',['filter' => 'teacherauthFilter']);
 });
